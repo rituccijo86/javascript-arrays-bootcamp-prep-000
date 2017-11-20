f20ff354072d607062efaa81221fd6b7b2633aee
John R <john.l.ritucci@gmail.com>
2017-11-20 22:28:30 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,36 @@
+var ingredient1 = "bread"
+var ingredient2 = "mild cheese"
+var ingredient3 = "sharp cheese"
+var ingredient4 = "butter"
+var ingredient5 = "tomato"
+var ingredient6 = "garlic"
+
+var chocolateBars = ['snickers','hundred grand','kitkat','skittles']
+
+function addElementToBeginningOfArray(array,element){
+
+}
+
+function destructivelyAddElementToBeginningOfArray(array,element){
+
+}
+
+function addElementToEndOfArray(array,element){
+
+}
+
+function destructivelyRemoveElementFromEndOfArray(array,element){
+
+}
+
+function accessElementInArray(array, index){
+
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+
+}
+
+function removeElementFromEndOfArray(array){
+
+}
